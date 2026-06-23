7d710d622f04cf386e2db5b11a2151820f434e32
Runatkk <runa.wahahahaha@gmail.com>
2026-06-23 21:36:15 +0900

    to marge

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true, // 💡 GitHub Pagesで画像を表示させるために必須
   },
+  // 💡 【重要】GitHub Pagesの階層（リポジトリ名）を正しく認識させる設定を追加します
+  basePath: '/bilingual-clinic-websites',
+  assetPrefix: '/bilingual-clinic-websites',
 };
 
 export default nextConfig;
