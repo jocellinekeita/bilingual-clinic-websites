b6c01592606ccee8915d9251e5d36332e7b2cbbf
Runatkk <runa.wahahahaha@gmail.com>
2026-06-23 23:11:51 +0900

    working on img display

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/bilingual-clinic-websites' : '',
+  basePath: isProd ? '/bilingual-clinic-websites' : '',
   env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '/bilingual-clinic-websites' : '',
+    NEXT_PUBLIC_BASE_PATH: isProd ? '/bilingual-clinic-websites' : '',
   },
   images: {
     unoptimized: true,
